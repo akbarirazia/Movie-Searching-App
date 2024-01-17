2fc92bb25e817450b606ea144699bfd941e621b3
RaziaAkbari <akbariharis14@gmail.com>
2024-01-17 09:17:29 +0430

    http request changed to https

--- src/Data/ApiCall.js
+++ src/Data/ApiCall.js
@@ -1,5 +1,5 @@
 export function ApiCall(name) {
-  const URL = `http://www.omdbapi.com/?t=${name}&apikey=528abed3`
+  const URL = `https://www.omdbapi.com/?t=${name}&apikey=528abed3`
   return fetch(URL)
     .then((res) => {
       if (!res.ok) {
@@ -15,7 +15,3 @@ export function ApiCall(name) {
       console.error("Error: ", error)
     })
 }
-
-console.log(ApiCall("The Nun"))
-console.log(ApiCall("Squid Game"))
-console.log(ApiCall("joker"))
