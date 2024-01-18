25fa29f5e4dcf694dbe5efe0cb0dcbbc015c7afd
RaziaAkbari <akbariharis14@gmail.com>
2024-01-18 14:05:17 +0430

    enhancement

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Placeholder() {
   return (
     <div className="py-5">
-      <p className="text-center mx-auto my-0 w-1/2 ">
+      <p className="text-center mx-auto my-0  lg:w-1/2 w-6/7 ">
         Search about any type of Movie and learn about it here though our
         website!
       </p>
